e6b12a3993cc023186e4314190f28fca8cada6fb
Max Lv <max.c.lv@gmail.com>
2015-12-23 13:09:06 +0800

    Update MD

--- app/src/main/java/io/github/baoliandeng/ui/MainActivity.java
+++ app/src/main/java/io/github/baoliandeng/ui/MainActivity.java
@@ -16,21 +16,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.*;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+
 import io.github.baoliandeng.R;
 import io.github.baoliandeng.core.LocalVpnService;
 
 import java.io.File;
 import java.util.Calendar;
 
-public class MainActivity extends Activity implements
+public class MainActivity extends ActionBarActivity implements
         OnCheckedChangeListener,
         LocalVpnService.onStatusChangedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String CONFIG_URL_KEY = "CONFIG_URL_KEY";
     private static final int START_VPN_SERVICE_REQUEST_CODE = 1985;
     private static String GL_HISTORY_LOGS;
-    private Switch switchProxy;
+    private SwitchCompat switchProxy;
     private TextView textViewLog;
     private ScrollView scrollViewLog;
     private Calendar mCalendar;
@@ -40,6 +43,9 @@ public class MainActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         scrollViewLog = (ScrollView) findViewById(R.id.scrollViewLog);
         textViewLog = (TextView) findViewById(R.id.textViewLog);
 
@@ -48,18 +54,6 @@ public class MainActivity extends Activity implements
 
         mCalendar = Calendar.getInstance();
         LocalVpnService.addOnStatusChangedListener(this);
-    }
-
-    String readConfigUrl() {
-        SharedPreferences preferences = getSharedPreferences("BaoLianDeng", MODE_PRIVATE);
-        return preferences.getString(CONFIG_URL_KEY, "");
-    }
-
-    void setConfigUrl(String configUrl) {
-        SharedPreferences preferences = getSharedPreferences("BaoLianDeng", MODE_PRIVATE);
-        Editor editor = preferences.edit();
-        editor.putString(CONFIG_URL_KEY, configUrl);
-        editor.commit();
     }
 
     String getVersionName() {
@@ -182,7 +176,7 @@ public class MainActivity extends Activity implements
             return false;
         }
 
-        switchProxy = (Switch) menuItem.getActionView();
+        switchProxy = (SwitchCompat) menuItem.getActionView();
         if (switchProxy == null) {
             return false;
         }
@@ -202,28 +196,6 @@ public class MainActivity extends Activity implements
                         .setMessage(R.string.about_info)
                         .setPositiveButton(R.string.btn_ok, null)
                         .show();
-                return true;
-            case R.id.menu_item_exit:
-                if (!LocalVpnService.IsRunning) {
-                    finish();
-                    return true;
-                }
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.menu_item_exit)
-                        .setMessage(R.string.exit_confirm_info)
-                        .setPositiveButton(R.string.btn_ok, new OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                LocalVpnService.IsRunning = false;
-                                LocalVpnService.Instance.disconnectVPN();
-                                stopService(new Intent(MainActivity.this, LocalVpnService.class));
-                                System.runFinalization();
-                                System.exit(0);
-                            }
-                        })
-                        .setNegativeButton(R.string.btn_cancel, null)
-                        .show();
-
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
